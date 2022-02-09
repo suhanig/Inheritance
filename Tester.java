6068ad82c953b6bf3f4cec95e0dc07863f5049f9
suhanig <60145970+suhanig@users.noreply.github.com>
2022-02-08 23:43:06 -0500

    submission for 1.9.2022

---
+++ Tester.java
@@ -0,0 +1,14 @@
+//tester for Inheritance
+
+public class Tester {
+
+	public static void main (String [] args ) {
+
+		Scholar scholar1 = new Scholar ("Sarah", 1995, "Neuroscience");
+		Scholar scholar2 = new Scholar ("Edward", 2001, "Business");
+		Instructor instructor1 = new Instructor ("Anna", 1993, "Economics", 20000);
+		Instructor instructor2 = new Instructor ("Mike", 2003, "Nursing", 10000);
+
+
+	} //closes psvm
+}
